6726fe9721ad36688096c84208357e31bba55c35
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-02-14 17:26:25 +0000

    Implement Shuffles
    Implements Shuffles:
    -Fisher-Yates
    -Riffle
    -None

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+        
+        List<Card> cards;
 
         public Pack()
         {
@@ -17,8 +19,21 @@ namespace CMP1903M_A01_2223
 
         public static bool shuffleCardPack(int typeOfShuffle)
         {
+            //Fisher-Yates Shuffle
+            //Riffle Shuffle
+            //No Shuffle
             //Shuffles the pack based on the type of shuffle
-
+            if (typeOfShuffle == 1)
+            {
+                return Program.PACK.fisherShatesShuffle();
+               
+            } else if (typeOfShuffle == 2)
+            {
+                return Program.PACK.riffleShuffle();
+            } else
+            {
+                return Program.PACK.noShuffle(); 
+            }
         }
         public static Card deal()
         {
@@ -29,5 +44,43 @@ namespace CMP1903M_A01_2223
         {
             //Deals the number of cards specified by 'amount'
         }
+
+        public bool riffleShuffle()
+        {
+            var packSize = cards.Count - 1;
+            if(packSize % 2 != 0 && packSize >= 2)
+            {
+                return false;
+            }
+            var leftPack = cards.GetRange(0, (packSize / 2));
+            var rightPack = cards.GetRange((packSize/2), packSize);
+            var newPack = new List<Card>();
+            for(var i = 0; i < (packSize / 2); i++)
+            {
+                newPack.Add(leftPack[i]);
+                newPack.Add(rightPack[i]);
+            }
+            cards = newPack;
+            return true;             
+        }
+
+        public bool noShuffle()
+        { 
+            return false; 
+        }
+
+        public bool fisherShatesShuffle()
+        {
+            foreach (Card card in cards)
+            {
+                var index = cards.IndexOf(card);
+                var randIndex = Program.RANDOM.Next(0, (cards.Count - 1));
+                var tempVar = cards[randIndex];
+                cards[randIndex] = cards[index];
+                cards[index] = tempVar;
+            }
+            return true;
+        }
+
     }
 }
--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -8,6 +8,9 @@ namespace CMP1903M_A01_2223
 {
     class Program
     {
+        public static Pack PACK = new Pack();
+        public static Random RANDOM = new Random();   
+
         static void Main(string[] args)
         {
 
