759b9e5360a99c8ee2627dfa99c322336ad62cde
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-03-08 15:39:36 +0000

    Initialised the pack and added Dealing
    created the initial pack of cards and added the function to deal single and multiple cards

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -9,43 +10,52 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        
-        List<Card> cards;
+        readonly List<Card> cards = new List<Card>();
 
         public Pack()
-        {
-            //Initialise the card pack here
-        }
-
-        public static bool shuffleCardPack(int typeOfShuffle)
-        {
-            //Fisher-Yates Shuffle
-            //Riffle Shuffle
-            //No Shuffle
-            //Shuffles the pack based on the type of shuffle
-            if (typeOfShuffle == 1)
-            {
-                return Program.PACK.fisherShatesShuffle();
-               
-            } else if (typeOfShuffle == 2)
-            {
-                return Program.PACK.riffleShuffle();
-            } else
-            {
-                return Program.PACK.noShuffle(); 
+        {for (int Value = 1; Value <= 13; Value++)
+            {for (int Suit = 1; Suit <= 4; Suit++)
+                {Card card = new Card();
+                    card.card_val = Value;
+                    card.card_suit = Suit;
+                    cards.Add(card);}
             }
         }
+
+           
+
+
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {if (typeOfShuffle == 1)
+            {return Program.pack1.fisherShatesShuffle();} 
+            else if (typeOfShuffle == 2)
+            {return Program.pack2.riffleShuffle();} 
+            else
+            {return Program.pack3.noShuffle(); }
+        }
+
         public static Card deal()
         {
-            //Deals one card
-
+            var packSize = cards.Count - 1;
+            Console.WriteLine(cards[packSize]);
+            Card deal_card = cards[packSize];
+            cards.RemoveAt(packSize);
+            return deal_card;
         }
+
         public static List<Card> dealCard(int amount)
         {
-            //Deals the number of cards specified by 'amount'
+            List<Card> cards = new List<Card>();
+            var packSize = cards.Count;
+            { for (int i = 0; i < amount; i++)
+                { cards.Add(cards[packSize - i]);
+                    cards.RemoveAt(packSize - i);
+                }
+            return cards;
         }
 
-        public bool riffleShuffle()
+            public bool riffleShuffle()
         {
             var packSize = cards.Count - 1;
             if(packSize % 2 != 0 && packSize >= 2)
