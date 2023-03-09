1c5f8e044818557b9bbe976bd32da16beb898f43
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-03-09 02:41:19 +0000

    Implemented Small Line Reductions
    Removed some unnecessary blank lines.

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -6,7 +6,6 @@ namespace CMP1903M_A01_2223
     {
         public static Pack PACK = new Pack(); //initialises the main pack of cards
         public static Random RANDOM = new Random(); //generates a random number when required
-
         static void Main(string[] args)
         {
             Testing testing = new Testing(); //declares the testing class as an object
